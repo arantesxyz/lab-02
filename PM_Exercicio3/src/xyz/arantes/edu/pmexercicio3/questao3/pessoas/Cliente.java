889b8b5e40c54f5cb31f7cb6dc2e6b0cf4f0ee76
arantesxyz <gustavo@arantes.xyz>
2019-03-17 20:42:37 -0300

    O cliente agora funciona tanto com pessoa fisica quanto juridica.

--- PM_Exercicio3/src/xyz/arantes/edu/pmexercicio3/questao3/pessoas/Cliente.java
+++ PM_Exercicio3/src/xyz/arantes/edu/pmexercicio3/questao3/pessoas/Cliente.java
@@ -2,16 +2,30 @@ package xyz.arantes.edu.pmexercicio3.questao3.pessoas;
 
 public class Cliente {
 
-    private Pessoa pessoa;
+    private PessoaFisica pessoaF = null;
+    private PessoaJuridica pessoaJ = null;
     private double limiteDeCredito;
 
-    public Cliente(Pessoa pessoa, double limiteDeCredito){
-        this.pessoa = pessoa;
+    public Cliente(PessoaFisica pessoa, double limiteDeCredito){
+        this.pessoaF = pessoa;
         this.limiteDeCredito = limiteDeCredito;
     }
 
-    public Pessoa getPessoa(){
-        return pessoa;
+    public Cliente(PessoaJuridica pessoa, double limiteDeCredito){
+        this.pessoaJ = pessoa;
+        this.limiteDeCredito = limiteDeCredito;
+    }
+
+    public boolean isJuridica(){
+        return pessoaJ == null;
+    }
+
+    public PessoaFisica getPessoaFisica(){
+        return pessoaF;
+    }
+
+    public PessoaJuridica getPessoaJuridica(){
+        return pessoaJ;
     }
 
     public double getLimiteDeCredito(){
